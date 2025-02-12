fcd66e27423db8fcf3459f8f21c609d7d6ee8d0f
fang-yin YEH <fangyin.yvonne@gmail.com>
2025-02-12 11:33:38 +0000

    int table for all 4 types of ads

---
+++ models/intermetiate/int_campaigns.sql
@@ -0,0 +1,16 @@
+SELECT * 
+FROM {{ ref('stg_gz_raw_data__adwords') }}
+
+UNION ALL
+
+SELECT * 
+FROM {{ ref('stg_gz_raw_data__bing') }}
+
+UNION ALL
+
+SELECT * 
+FROM {{ ref('stg_gz_raw_data__criteo') }}
+UNION ALL
+
+SELECT * 
+FROM {{ ref('stg_gz_raw_data__facebook') }}
